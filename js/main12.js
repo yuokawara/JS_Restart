0e6152d0d5bbbedd704961a47bd53ea1e75535e8
大河原悠 <solbonta1@gmail.com>
2020-02-03 22:22:47 +0900

    jquery movie追加 20200203

---
+++ js/main12.js
@@ -0,0 +1,26 @@
+// DOMの準備後に実行する内容
+$(function() {
+    // 変数の初期化
+    var video = $("#video").get(0);
+
+    // 再生
+    var play = function() {
+        video.play();
+    };
+
+    // 一時停止
+    var pause = function() {
+        video.pause();
+    };
+
+    // 停止
+    var stop = function() {
+        video.pause();
+        video.currentTime = 0;
+    };
+
+    // buttonのイベントを登録
+    $("#btnPlay").click(play);
+    $("#btnPause").click(pause);
+    $("#btnStop").click(stop);
+});
